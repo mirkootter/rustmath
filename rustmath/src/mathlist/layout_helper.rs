1afd6ff4179a2cb3477cf77f0b78992d95e9eba2
Jan-Mirko Otter <janmirko.otter@googlemail.com>
2023-06-22 14:59:35 +0200

    better script shifts

--- rustmath/src/mathlist/layout_helper.rs
+++ rustmath/src/mathlist/layout_helper.rs
@@ -3,8 +3,20 @@ use crate::{common::font_params::ScriptParams, layout::Node};
 pub fn calculate_script_shifts<G: crate::common::Glyph>(
     params: &ScriptParams,
     _nucleus: &Option<Node<G>>,
-    _subscript: &Option<Node<G>>,
-    _superscript: &Option<Node<G>>,
+    subscript: &Option<Node<G>>,
+    superscript: &Option<Node<G>>,
 ) -> (f32, f32) {
-    (-params.subscript.shift_down, params.superscript.shift_up)
+    let mut vshift_down = params.subscript.shift_down;
+    if let Some(subscript) = subscript {
+        vshift_down = vshift_down.max(subscript.height() - params.subscript.top_max);
+        // TODO: Baseline drop?
+    }
+
+    let mut vshift_up = params.superscript.shift_up;
+    if let Some(superscript) = superscript {
+        vshift_up = vshift_up.max(superscript.depth() + params.superscript.bottom_min);
+        // TODO: Baseline drop?
+    }
+
+    (-vshift_down, vshift_up)
 }
