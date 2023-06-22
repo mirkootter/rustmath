8c81b2ffac37517f3785496f989afa0d616495fc
Jan-Mirko Otter <janmirko.otter@googlemail.com>
2023-06-22 15:36:20 +0200

    script-layout: baseline drop

--- rustmath/src/mathlist/layout_helper.rs
+++ rustmath/src/mathlist/layout_helper.rs
@@ -2,20 +2,28 @@ use crate::{common::font_params::ScriptParams, layout::Node};
 
 pub fn calculate_script_shifts<G: crate::common::Glyph>(
     params: &ScriptParams,
-    _nucleus: &Option<Node<G>>,
+    nucleus: &Option<Node<G>>,
     subscript: &Option<Node<G>>,
     superscript: &Option<Node<G>>,
 ) -> (f32, f32) {
     let mut vshift_down = params.subscript.shift_down;
     if let Some(subscript) = subscript {
         vshift_down = vshift_down.max(subscript.height() - params.subscript.top_max);
-        // TODO: Baseline drop?
+
+        // TODO: The following should only be applied on subformulars, operators, ...
+        if let Some(base) = nucleus {
+            vshift_down = vshift_down.max(base.depth() + params.subscript.baseline_drop_min);
+        }
     }
 
     let mut vshift_up = params.superscript.shift_up;
     if let Some(superscript) = superscript {
         vshift_up = vshift_up.max(superscript.depth() + params.superscript.bottom_min);
-        // TODO: Baseline drop?
+
+        // TODO: The following should only be applied on subformulars, operators, ...
+        if let Some(base) = nucleus {
+            vshift_up = vshift_up.max(base.height() - params.superscript.baseline_drop_max);
+        }
     }
 
     (-vshift_down, vshift_up)
